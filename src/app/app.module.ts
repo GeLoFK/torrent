68ef8e9c4bf266df2fdbc0fd225479b92b66cf8e
GeLoFK <flamaster8787@gmail.com>
2023-07-06 01:29:15 +0300

    Test work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ClassicComponent } from './classic/classic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ProductComponent,
+    ClassicComponent
   ],
   imports: [
     BrowserModule,
