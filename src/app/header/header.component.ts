293f8d4fc8997a2df6542e2cb0d801b78a05d26a
GeLoFK <flamaster8787@gmail.com>
2023-07-06 23:21:18 +0300

    Add ts code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+interface Language {
+  id: string;
+  hreflang: string;
+  label: string;
+  href: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+
+  animations: [
+    trigger('dropdownAnimation', [
+      state('void', style({ opacity: 0, transform: 'translateY(-20px)' })),
+      state('*', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition('void => *', animate('200ms ease-in')),
+      transition('* => void', animate('200ms ease-out'))
+    ])
+  ]
 })
 export class HeaderComponent {
   isDropdownOpen = false;
+
+  languages: Language[] = [
+    { id: 'mainnav-lang-en', hreflang: 'en', label: 'English', href: '/' },
+    { id: 'mainnav-lang-de', hreflang: 'de', label: 'Deutsch', href: '/intl/de/' },
+    { id: 'mainnav-lang-es', hreflang: 'es', label: 'Español', href: '/intl/es/' },
+    { id: 'mainnav-lang-es', hreflang: 'pl', label: 'Poland', href: '/intl/pl/' },
+    { id: 'mainnav-lang-es', hreflang: 'ru', label: 'Русский', href: '/intl/ru/' },
+    { id: 'mainnav-lang-es', hreflang: 'ua', label: 'Український', href: '/intl/ua/' }
+  ];
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
